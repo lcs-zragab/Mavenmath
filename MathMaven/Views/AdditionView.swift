726b9c82f2600926c957e8310cb7a327f1953e74
Russell Gordon <rgordon@lcs.on.ca>
2023-05-24 07:45:35 -0400

    User can now provide an answer for addition.

--- MathMaven/Views/AdditionView.swift
+++ MathMaven/Views/AdditionView.swift
@@ -10,8 +10,19 @@ import SwiftUI
 struct AdditionView: View {
     
     // MARK: Stored properties
+    
+    // The numbers to be added
     @State var augend = Int.random(in: 1...72)
     @State var addend = Int.random(in: 1...72)
+    
+    // Holds whatever input was provided by the user
+    @State var input = ""
+    
+    // Has the user's answer been checked?
+    @State var answerChecked = false
+    
+    // Was the user's given answer correct?
+    @State var answerCorrect = false
     
     // MARK: Computed properties
     var body: some View {
@@ -34,7 +45,35 @@ struct AdditionView: View {
                 Divider()
             }
             .padding(.horizontal)
-            
+
+            // 2. Accept answer
+            HStack {
+
+                ZStack {
+                    
+                    // Only show this when the answer was found to be correct
+                    if answerCorrect == true {
+                        Image(systemName: "checkmark.circle")
+                            .foregroundColor(.green)
+                    }
+                    
+                    // Show this when the answer was checked and found to be false
+                    if answerChecked == true && answerCorrect == false {
+                        Image(systemName: "x.square")
+                            .foregroundColor(.red)
+                    }
+                }
+                
+                Spacer()
+                
+                TextField("",
+                          text: $input)
+                    .multilineTextAlignment(.trailing)
+            }
+            .padding(.horizontal)
+
+            // Push interface up to top of screen
+            Spacer()
         }
         .font(Font.custom("SF Pro", size: 64))
     }
