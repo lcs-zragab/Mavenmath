acadc501c65c1ad104768fca8c48e5ea8adaddc7
Russell Gordon <rgordon@lcs.on.ca>
2023-05-24 07:13:36 -0400

    Initial Commit

---
+++ MathMaven/MathMavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathMavenApp.swift
+//  MathMaven
+//
+//  Created by Russell Gordon on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct MathMavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
